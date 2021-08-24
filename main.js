c1cf285514adc861e7dda08f84f0a5cb4804bf28
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-08-24 14:37:47 +0200

    Expand function to accept strings of limited length and add tests for it

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-const stringLength = (string) => string.length;
+const stringLength = (string) => {
+  if (string.length < 1 || string.length > 10 ) {
+    throw new Error( 'Please use string that has length between 1 and 10 characters');
+  }
+  return string.length;
+}
+
 
 module.exports = stringLength;
--- main.test.js
+++ main.test.js
@@ -3,4 +3,15 @@ const stringLength = require('./main');
 it('Count length of string', () => {
   expect(stringLength("string")).toBe(6);
   expect(stringLength("longString")).toBe(10);
-}) 
+});
+
+it('Length is between 1 and 10 characters', () => {
+  expect(() => {
+    stringLength('')
+  })
+  .toThrow('Please use string that has length between 1 and 10 characters');
+  expect(() => {
+    stringLength('moreThanTenLetters')
+  })
+  .toThrow('Please use string that has length between 1 and 10 characters')
+})
