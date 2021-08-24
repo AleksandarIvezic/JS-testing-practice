520b8a8fe78c72db35a610db5a7e3a77b64370c8
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-08-24 16:10:25 +0200

    Add test for checking type of input in capitalize

--- capitalize.test.js
+++ capitalize.test.js
@@ -4,5 +4,8 @@ const capitalize = functions.capitalize;
 describe('My capitalize function', () => {
   it('takes string as an argument and return it with the first letter capitalized', () => {
     expect(capitalize('string')).toBe('String');
+  });
+  it('throws error if you try to put wrong type of input', () => {
+    expect(() => capitalize(644)).toThrow('Please enter the accurate input')
   })
 });
